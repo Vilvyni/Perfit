b0a43e930a617fc369c9dce51bd01594a10107fb
yy-suisse <58217823+yy-suisse@users.noreply.github.com>
2022-01-06 23:24:01 +0100

    add intent for gallery

--- mobile/src/main/java/com/epfl/esl/myapplication/AddClosetFragment.kt
+++ mobile/src/main/java/com/epfl/esl/myapplication/AddClosetFragment.kt
@@ -1,32 +1,46 @@
 package com.epfl.esl.myapplication
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import com.epfl.esl.myapplication.databinding.FragmentAddClosetBinding
-import com.epfl.esl.myapplication.databinding.FragmentLoginBinding
 
 class AddClosetFragment : Fragment() {
 
     private lateinit var binding: FragmentAddClosetBinding
     private lateinit var viewModel: AddClosetViewModel
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {binding =
-        DataBindingUtil.inflate(inflater, R.layout.fragment_add_closet,
-            container, false)
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_add_closet,
+                container, false)
+
 
 //        ADD_CLOSET to CLOSET
         binding.add.setOnClickListener {view : View ->
             Navigation.findNavController(view)
                 .navigate(R.id.action_addClosetFragment_to_closetFragment)
+        }
+
+        binding.newItem.setOnClickListener {
+
+            val imgIntent = Intent(Intent.ACTION_GET_CONTENT)
+            imgIntent.setType("image/*")
+            resultLauncher.launch(imgIntent)
         }
 
         return binding.root
@@ -38,4 +52,13 @@ class AddClosetFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val imageUri: Uri? = result.data?.data
+                // bind the received image to the image view
+                binding.newItem.setImageURI(imageUri)
+            }
+
+        }
 }
